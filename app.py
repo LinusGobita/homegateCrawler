86a18a7ba6d3b668374ac5c055676bdbed2a5d0d
LinusGobita <90457320+LinusGobita@users.noreply.github.com>
2022-01-09 17:31:17 +0100

    Version 1.0

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from urllib.request import Request, urlopen
+
 import requests
 from tqdm import tqdm
 
@@ -5,11 +7,19 @@ base_url = 'https://www.homegate.ch/kaufen/'
 id = 3001609001
 url = base_url + str(id)
 
-response = requests.get(url)
+
+
+
+response = urlopen(url)
 print(response)
 #    soup = BeautifulSoup(response.content, "html.parser")
 #    print(soup.prettify())
 print(url)
+
+req = Request('http://www.cmegroup.com/trading/products/#sortField=oi&sortAsc=false&venues=3&page=1&cleared=1&group=1', headers={'User-Agent': 'Mozilla/5.0'})
+webpage = urlopen(req).read()
+print(webpage)
+
 with open(str(id), "wd") as handle:
     for data in tqdm(response.iter_content()):
         handle.write(data)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import time, random, os, logging
+import urllib
+from urllib.request import Request, urlopen
 from datetime import datetime
 import requests
 from tqdm import tqdm
@@ -7,24 +9,32 @@ from urllib.parse import urljoin
 
 
 base_url = 'https://www.homegate.ch/kaufen/'
-id = 3001609010
+id = 300160908
+url = base_url + str(id)
+div_headline_identifier = {"class":"schlagzeilen-content schlagzeilen-overview"}
 
-sub_dir = 'HomegateCrawlerDB'
-sub_dir_logs = 'logs'
-log_file_name = 'homegate-'+str(id)
-timeout_duration = 5
-num_latest_downloads = 1000
-sleep_min = 1
-sleep_max = 10
-#div_headline_identifier = {"class":"schlagzeilen-content schlagzeilen-overview"}
-
+########### sleep ###########
+sleep_min = 5
+sleep_max = 15
 sleeptimes = list(range(sleep_min,sleep_max,1))
 
+########### Data Name ###########
+sub_dir = 'HomegateCrawlerDB'
+sub_dir_logs = 'logs'
+sub_dir_data = 'data'
+log_file_name = 'homegate'
+data_file_name = 'homegate-'+str(id)
+timeout_duration = 5
+num_latest_downloads = 1000
+########### Data  ###########
 abs_path = os.path.join(os.getcwd(),sub_dir)
 abs_path_logs = os.path.join(abs_path,sub_dir_logs)
-abs_path_data = os.path.join(abs_path,log_file_name)
+abs_path_data = os.path.join(abs_path,sub_dir_data)
+#create path
 if not os.path.exists(abs_path_logs):
     os.makedirs(abs_path_logs)
+if not os.path.exists(abs_path_data):
+    os.makedirs(abs_path_data)
 
 latest_downloads = os.listdir(abs_path)
 total_downloaded = len(latest_downloads)-1
@@ -37,7 +47,6 @@ last_batch = 0
 
 while True:
     id += 1
-    print(id)
     date_str = datetime.now().strftime("%Y%m%d")
     log_file = os.path.join(abs_path_logs, f'{log_file_name}_{date_str}.log')
     logging.basicConfig(
@@ -53,14 +62,22 @@ while True:
     time.sleep(sleeptime)
     last_batch = 0
 
-    response = requests.get(base_url + str(id))
-    print(base_url + str(id))
-    print(response)
+
+    file_path = os.path.join(abs_path_data, f'Homegate_{id}.html')
+    #for urllib.error.HTTPError: HTTP Error 999: No Hacking
+#    req = urllib.request.Request(
+#        url,
+#        data=None,
+#        headers={'User-Agent': ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) "
+#                                "AppleWebKit/537.36 (KHTML, like Gecko) "
+#                                "Chrome/35.0.1916.47 Safari/537.36")})
+
     try:
-        file_path = os.path.join(abs_path_data, f'Homegate_{id}.webloc')
-        with open(file_path, 'wb') as handle:
-            for data in tqdm(response.iter_content()):
-                handle.write(data)
+        #Get URL and save it
+        response = requests.get(base_url + str(id))
+        string = response.text
+        with open(file_path, "w") as handle:
+            handle.write(string)
 
         total_downloaded += 1
         last_batch += 1
