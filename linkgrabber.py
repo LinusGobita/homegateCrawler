06248f3043af2c97ae384152a6ef6566b233f329
Tobias Gross <tobias.gross@gross-hosting.com>
2022-01-10 23:35:57 +0100

    Full Grabber V2

--- linkgrabber.py
+++ linkgrabber.py
@@ -1,9 +1,16 @@
+import random
+import main
+from time import sleep
+
 from bs4 import BeautifulSoup
 import requests
 
 if __name__ == "__main__":
     base_urls = ["https://www.homegate.ch/mieten/immobilien/land-schweiz/trefferliste?ep=",
                  "https://www.homegate.ch/kaufen/immobilien/land-schweiz/trefferliste?ep="]
+    sleep_min = 2
+    sleep_max = 5
+    sleeptimes = list(range(sleep_min, sleep_max, 1))
     for base_url in base_urls:
         page = 0
         while True:
@@ -15,7 +22,9 @@ if __name__ == "__main__":
             listingsList = listingsGroup.find_all("a", {"data-test": "result-list-item"})
 
             for listing in listingsList:
-                print("https://homegate.ch" + listing["href"])
-            # print(response.text)
+                main.get_listing("https://www.homegate.ch"+listing["href"])
+                sleepTime = random.choice(sleeptimes)
+                print("Sleeping " + str(sleepTime) + " Seconds...")
+                sleep(sleepTime)
             if len(listingsList) < 10:
                 break
--- main.py
+++ main.py
@@ -2,61 +2,36 @@ import time, random, os, logging
 from datetime import datetime
 import requests
 
-#Done IDs
+
+# Done IDs
 ###Probeläufe
-#Linus  | Macbook Pro   |    300160909 ca 40
-#Linus  | Macbook Pro   |    3001039264 - 3001039552
+# Linus  | Macbook Pro   |    300160909 ca 40
+# Linus  | Macbook Pro   |    3001039264 - 3001039552
 ###Real
-#Linus  | iMac          |    3001539264 - 3001539291
-#Linus  | iMac          |    3001582000 -
-#Linus  | Macbook Pro   |    3001611000 - 3001612560 -
+# Linus  | iMac          |    3001539264 - 3001539291
+# Linus  | iMac          |    3001582000 -
+# Linus  | Macbook Pro   |    3001611000 - 3001612560 -
 
 
-#3001614515
-#3001609557
-#3001615108
+# 3001614515
+# 3001609557
+# 3001615108
 
-
-
-base_url = 'https://www.homegate.ch/kaufen/'
-id = 3001612560
-url = base_url + str(id)
-div_headline_identifier = {"class":"schlagzeilen-content schlagzeilen-overview"}
-
-########### sleep ###########
-sleep_min = 5
-sleep_max = 20
-sleeptimes = list(range(sleep_min,sleep_max,1))
-
-########### Data Name ###########
-sub_dir = 'HomegateCrawlerDB'
-sub_dir_logs = 'logs'
-sub_dir_data = 'data'
-log_file_name = 'homegate'
-data_file_name = 'homegate-'+str(id)
-timeout_duration = 5
-num_latest_downloads = 1000
-########### Data  ###########
-abs_path = os.path.join(os.getcwd(),sub_dir)
-abs_path_logs = os.path.join(abs_path,sub_dir_logs)
-abs_path_data = os.path.join(abs_path,sub_dir_data)
-#create path
-if not os.path.exists(abs_path_logs):
-    os.makedirs(abs_path_logs)
-if not os.path.exists(abs_path_data):
-    os.makedirs(abs_path_data)
-
-latest_downloads = os.listdir(abs_path)
-total_downloaded = len(latest_downloads)-1
-total_timeouts = 0
-total_other = 0
-total_unknown_errors = 0
-last_batch = 0
-
-
-
-while True:
-    id += 1
+def get_listing(listingURL):
+    sub_dir = 'HomegateCrawlerDB'
+    sub_dir_logs = 'logs'
+    sub_dir_data = 'data'
+    log_file_name = 'homegate'
+    listingID = str.split(listingURL, "/")[-1]
+    data_file_name = 'homegate-' + str(id)
+    timeout_duration = 5
+    abs_path = os.path.join(os.getcwd(), sub_dir)
+    abs_path_logs = os.path.join(abs_path, sub_dir_logs)
+    abs_path_data = os.path.join(abs_path, sub_dir_data)
+    if not os.path.exists(abs_path_logs):
+        os.makedirs(abs_path_logs)
+    if not os.path.exists(abs_path_data):
+        os.makedirs(abs_path_data)
     date_str = datetime.now().strftime("%Y%m%d")
     log_file = os.path.join(abs_path_logs, f'{log_file_name}_{date_str}.log')
     logging.basicConfig(
@@ -65,41 +40,15 @@ while True:
         filemode="a+",
         format="%(asctime)-15s %(message)s"
     )
-
-    sleeptime = random.choice(sleeptimes)
-    print(f'Sleeping: {sleeptime} \tLast bach-size: {last_batch}')
-    logging.info(f'Sleeping: {sleeptime} \tLast bach-size: {last_batch}')
-    time.sleep(sleeptime)
-    last_batch = 0
-
-
-    file_path = os.path.join(abs_path_data, f'Homegate_{id}.html')
-    #for urllib.error.HTTPError: HTTP Error 999: No Hacking
-#    req = urllib.request.Request(
-#        url,
-#        data=None,
-#        headers={'User-Agent': ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) "
-#                                "AppleWebKit/537.36 (KHTML, like Gecko) "
-#                                "Chrome/35.0.1916.47 Safari/537.36")})
-
+    file_path = os.path.join(abs_path_data, f'Homegate_{listingID}_{date_str}.html')
     try:
-        #Get URL and save it
-        response = requests.get(base_url + str(id))
+        # Get URL and save it
+        response = requests.get(listingURL)
         string = response.text
-        with open(file_path, "w") as handle:
+        with open(file_path, "w", encoding="utf-8") as handle:
             handle.write(string)
-
-        total_downloaded += 1
-        last_batch += 1
-        print(f'Downloaded ({total_downloaded})')
-        logging.info(f'Downloaded ({total_downloaded})')
-
-
-    except:
-        total_unknown_errors += 1
-        print(f'Unknown Error ({total_unknown_errors})')
-        logging.info(f'Unknown Error ({total_unknown_errors})')
-
-
-
-
+        print(f'Downloaded ({listingURL})')
+        logging.info(f'Downloaded ({listingURL})')
+    except requests.exceptions.Timeout:
+        print("Timeout Error")
+        quit(1)
